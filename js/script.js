63c5bf1c72a163d0c10d7b3a0da8d2b9fc0e0318
Henryn21 <45953538+Henryn21@users.noreply.github.com>
2020-07-05 20:58:39 -0700

    Testing api response

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ xhr.onreadystatechange=function(){//when state changes
 //public api test: https://randomuser.me/api/
 //google places API
 ////maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E
-xhr.open("GET", 'maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E');
+xhr.open("GET", `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E?input=KFC`);
 xhr.send();
 
 //display results
