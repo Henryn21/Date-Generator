680e8d115be6300e1831e7a26814071f295be7ee
Henryn21 <45953538+Henryn21@users.noreply.github.com>
2020-07-28 00:55:27 -0700

    Re-wrote JS
    updated for dropdown instead of buttons
    started with an older js file (before it even worked)

--- js/script.js
+++ js/script.js
@@ -1,126 +1,59 @@
-  //get form data
-  //html elements, whole form, div holding food buttons, nodelist of each food button
+//get form data
 let form=document.querySelector("form");
-let foodButtonDiv=document.querySelector("#food");
-let foodButtons=document.querySelectorAll(".foodButton");
-let activityButtons=document.querySelectorAll(".activityButton");
+let apiKey="";//will create later to encrypt key
+let foodList=document.querySelector("#foodList");
+let activityList=document.querySelector("#activityList");
+//console.log(foodList.options[foodList.selectedIndex].value);
 
-let activity;
 
-//SELECTED BUTTON
-//buttons start un-selected
-
-//FOOD
-//select/deselect
-
-//add listener to each button
-for(let i=0;i<foodButtons.length;i++){
-    //when clicked
-    foodButtons[i].addEventListener("click", (e)=>{
-        //clear selected
-        for(let j=0;j<foodButtons.length;j++){
-            foodButtons[j].style.backgroundColor="#4E0250";
-        }
-        //select clicked
-        foodButtons[i].style.backgroundColor="blue";
-    });
-}
-//ACTIVITY
-//select/deselect
-
-//add listener to each button
-for(let i=0;i<activityButtons.length;i++){
-    //when clicked
-    activityButtons[i].addEventListener("click", (e)=>{
-        //clear selected
-        for(let j=0;j<activityButtons.length;j++){
-            activityButtons[j].style.backgroundColor="#4E0250";
-        }
-        //select clicked
-        activityButtons[i].style.backgroundColor="blue";
-    });
-}
-
-//FUNCTION FOR RETURNING SELECTED ANSWERS
-
-//For answer buttons
-let selected;
-function getAnswers(buttonList){
-    for(let i=0;i<buttonList.length;i++){
-        if(buttonList[i].style.backgroundColor=="blue"){
-            selected=buttonList[i].id;
-        }
-    }
-    return(selected)
-}
-
-//For Zip
-let zipcodeField=document.querySelector("#zip");
-function getZip(){
-    return(zipcodeField.value);
-}
-
-//When submit button clicked, all answers entered into variables
+//submit button
 form.addEventListener('submit',(e)=>{
     e.preventDefault();//dont "send"
-    zip=getZip();
-    food=getAnswers(foodButtons);
-    activity=getAnswers(activityButtons);
-    xhr.open("GET", `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${food}&key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E`);
+    //assign response to Strings
+    let food=foodList.options[foodList.selectedIndex].value;
+    let activity=activityList.options[activityList.selectedIndex].value;
+
+    console.log(food);
+    console.log(activity);
+
+    //create url to send
+    //food
+    let foodSearch=`maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E&query=${food}`;
+    //activity
+    let activitySearch=`maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E&query=${activity}`;
+
+    //send API request using responses
+    xhr.open("GET", foodSearch);
     xhr.send();
+
 });
-
-//SENDING TO API AND RESPONSE
-
-//Bug: Places api not giving right response
-//CORS issue, need to use a proxy?
-
 //enter form data to google api
-
-//list for returned objects(places)
 let apiElements=[];
 xhr= new XMLHttpRequest();//request object
 xhr.onreadystatechange=function(){//when state changes
     if(this.readyState===4 && this.status === 200) {//if done
         console.log("done");
-        //returned JSON object from api
         let data=JSON.parse(xhr.responseText);
         console.log(data);
         for(let i=0;i<data.results.length;i++){
-            apiElements[i]=data.results[i];
+            apiElements[i]=data.results[i];//list of results
         }
         console.log(apiElements);
         resultsButton.style.display="block";
     }
 }
-
-//API- GMAPS/PLACES JSON, from HTTP URL with parameters
 //public api test: https://randomuser.me/api/
 //google places API
-//BETTER BUT COSTS: TEXT SEARCH REQUEST
-//https://maps.googleapis.com/maps/api/place/textsearch/json?query=chicken&key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E
-
-//FREE BUT SIMPLE SEARCH: FIND PLACE REQUEST
-//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E
-//More parameters request
-//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E&input=KFC&inputtype=textquery&fields=rating,name,opening_hours
-//BUG: need to define food variable before request
-// xhr.open("GET", `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${food}&key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E`);
-// xhr.send();
+////maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyBYZMSmO_lrxaddKipkStuvMAvuaDJoU4E
 
 
 //display results
-
-//select results button
 let resultsButton=document.querySelector("#results");
-//hide
 resultsButton.style.display="none";
-//when clicked, 5 times(create list item, fill item's html with item at current index, add item to list)
 resultsButton.addEventListener("click",(e)=>{
     for(let i=0;i<5;i++){
         let resultItem=document.createElement("li");
-        resultItem.innerHTML=apiElements[i].name;
-        console.log(apiElements[i].name);
+        resultItem.innerHTML=apiElements[i];
         document.querySelector("#resultsList").appendChild(resultItem);
     }
     
